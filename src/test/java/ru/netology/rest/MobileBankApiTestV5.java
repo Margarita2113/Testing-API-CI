cde83be647e0804586ca903d96a35842788240dc
Oskina Margarita <rita_2113@mail.ru>
2023-07-18 20:29:29 +0300

    failed tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -1,12 +1,10 @@
 package ru.netology.rest;
 
 import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 
 class MobileBankApiTestV5 {
     @Test
@@ -19,7 +17,7 @@ class MobileBankApiTestV5 {
                 .when()
                 .get("/demo/accounts")
                 // Проверки
-                .then()
+                .then().log().all()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
                 .body("every{ it.balance >= 0 }", is(true))
